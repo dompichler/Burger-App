65e812de882e52175d6b5dcd074cc7e84bfb4639
dompichler <Wombart1#>
2020-10-05 16:51:28 +0200

    init comit

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import classes from './BuildControls.css'
+import BuildControl from './BuildControl/BuildControl'
+
+
+const controls = [
+  {label : 'Salad', type: 'salad'},
+  {label : 'Bacon', type: 'bacon'},
+  {label : 'Cheese', type: 'cheese'},
+  {label : 'Meat', type: 'meat'}
+];
+
+const buildControls = (props) => {
+  
+  return(
+   <div className={classes.BuildControls}>
+     <p>Current Price: <strong>{(Math.round(props.price * 100) / 100).toFixed(2)} €</strong></p>
+     
+     {controls.map(ctrl => (
+       <BuildControl
+         key={ctrl.label}
+         label={ ctrl.label}
+         added={() => props.ingredientAdded(ctrl.type)}
+         removed={() => props.ingredientRemoved(ctrl.type)}
+       />
+     ))}
+     <button className={classes.OrderButton}
+             disabled={!(props.purchaseable)}
+             onClick={props.ordered}
+     >{props.ordered} ORDER NOW</button>
+     
+   </div>
+   
+   
+   
+   
+  );
+  
+};
+
+export default buildControls;
